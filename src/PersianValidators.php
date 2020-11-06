01283e5b6e39276e61471670564e533f369ae74c
Amirhossein Banavi <ah.banavi@gmail.com>
2020-11-06 15:10:01 +0330

    fix preg match delimiter error

--- src/PersianValidators.php
+++ src/PersianValidators.php
@@ -129,7 +129,7 @@ class PersianValidators
      */
     public function validateIranianPostalCode($attribute, $value, $parameters)
     {
-        return preg_match("\b(?!(\d)\1{3})[13-9]{4}[1346-9]-?[013-9]{5}\b", $value);
+        return preg_match("/\b(?!(\d)\1{3})[13-9]{4}[1346-9]-?[013-9]{5}\b/", $value);
     }
 
     /**
